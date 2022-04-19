130e396fc86dd6b34968ec0e3e54914a5a77980c
Ji Hyuk Ma <t90laser93@gmail.com>
2022-04-19 17:56:59 +0900

    feat: dynamicIcon 컴포넌트 생성
    DynamicIcon 컴포넌트 생성하였습니다. 구체적으로 전달될 prop이나, 전달하는 두 개의 아이콘을 합쳐서 전달하는 게 나을지에 대한 논의가 추가로 필요할 듯 합니다.

---
+++ src/components/Icons/DynamicIcon.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function DynamicIcon(props) {
+  const { clickedIcon, unClickedIcon, size, color, onClick, isClicked } = props;
+  console.log(isClicked);
+
+  const DynamicIcon = isClicked ? clickedIcon : unClickedIcon;
+
+  let iconSize = size;
+  if (iconSize === "small") {
+    iconSize = "w-6 h-6";
+  } else if (iconSize === "medium") {
+    iconSize = "w-8 h-8";
+  } else if (iconSize === "large") {
+    iconSize = "w-10 h-10";
+  }
+
+  return <DynamicIcon className={`${iconSize} ${color}`}></DynamicIcon>;
+}
+
+DynamicIcon.propTypes = {
+  clickedIcon: PropTypes.func.isRequired,
+  unClickedIcon: PropTypes.func.isRequired,
+  size: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
+  isClicked: PropTypes.bool,
+};
--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import StaticIcon from "@component/Icons/StaticIcon";
 import { CgTwitter } from "react-icons/cg";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import DynamicIcon from "../../components/Icons/DynamicIcon";
 export default function MainPage() {
   return (
     <div>
       <div className="text-blue-500 ">안녕하세요</div>
       <StaticIcon icon={CgTwitter} size={"large"} color={"text-blue-500"} />
+      <DynamicIcon
+        clickedIcon={FaHeart}
+        unClickedIcon={FaRegHeart}
+        size={"large"}
+        color={"text-blue-500"}
+        isClicked={true}
+      />
     </div>
   );
 }

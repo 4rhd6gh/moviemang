19dd2ce8cd49e5d9eb2a1d96fc56ee7392c3acf5
bgradecoding <iinee12@kakao.com>
2022-04-20 23:27:20 +0900

    feat(tag.jsx): tag 컴포넌트 작성 완료

---
+++ src/components/tag/Tag.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function Tag(props) {
+  const { text, size, onClick } = props;
+  let textSize = size;
+  if (textSize === "small") {
+    textSize = "text-xs";
+  } else if (textSize === "medium") {
+    textSize = "text-lg";
+  } else if (textSize === "large") {
+    textSize = "text-xl";
+  }
+
+  return (
+    <div
+      className={`${textSize} text-gray-500`}
+      onClick={onClick}
+    >{`#${text}`}</div>
+  );
+}
+
+Tag.propTypes = {
+  text: PropTypes.string.isRequired,
+  size: PropTypes.string,
+  onClick: PropTypes.func,
+};

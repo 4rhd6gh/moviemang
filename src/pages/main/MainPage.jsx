eda80424742297bc8dc8e1e9f77a5e0cd1f5e6a0
Ji Hyuk Ma <t90laser93@gmail.com>
2022-04-20 13:38:43 +0900

    fix: conflict 해결 수정
    github merge 화면에서 conflict 수정 시에 뭔가 실수를 한거같습니다.. 불필요한 코드가 남아있어서 해당 부분 수정해서 커밋합니다!

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -12,36 +12,6 @@ export default function MainPage() {
     <div>
       <div className="text-blue-500 ">안녕하세요</div>
       <StaticIcon icon={CgTwitter} size={"large"} color={"text-blue-500"} />
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <DynamicIcon
-        clickedIcon={FaHeart}
-        unClickedIcon={FaRegHeart}
-        size={"large"}
-        color={"text-blue-500"}
-        isClicked={true}
-=======
-=======
-
->>>>>>> d631536a9e7eb4dd8cbbc688be26478d6a0baae7
-      <Button
-        text="first"
-        type="button"
-        color={"text-orange-200 bg-indigo-400 hover:bg-indigo-500 "}
-        border={"rounded-lg"}
-      />
-      <Button
-        text="second"
-        type="button"
-        color={"text-indigo-900 hover:bg-indigo-100"}
-        border={"border-2 border-indigo-900 rounded-full"}
-        focus={"focus:outline-none focus:ring-1 focus:ring-offset-2"}
-        onClick={onClick}
-<<<<<<< HEAD
->>>>>>> main
-=======
->>>>>>> d631536a9e7eb4dd8cbbc688be26478d6a0baae7
-      />
     </div>
   );
 }

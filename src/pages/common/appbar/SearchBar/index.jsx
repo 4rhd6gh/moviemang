edd62e92b945ca3ae8123d4984981e6a56f17f4d
ssun-aa <ssk6200@daum.net>
2022-04-22 00:04:40 +0900

    feat: searchBar 폴더링 변경 및 파일 이름 변경

--- src/pages/common/appbar/SearchBar/index.jsx
+++ src/pages/common/appbar/SearchBar/index.jsx
@@ -4,16 +4,17 @@ import StaticIcon from "@component/Icons/StaticIcon";
 import { FaSearch } from "react-icons/fa";
 
 export default function SearchBar(props) {
-  const { size } = props;
   return (
-    <div className="relative">
+    <div className="relative w-96 ml-48">
       <input
         type="search"
         name="search"
         placeholder="Search"
-        className="sm:flex w-3/12 max-w-sm bg-white h-10 px-5 pr-10 rounded-full border-4 text-sm focus:outline-none"
+        className="w-full bg-white h-10 px-5 pr-10 rounded-full border-2 text-sm focus:outline-none"
       />
-      <StaticIcon icon={FaSearch} size={"small"} color={"text-gray-200"} />
+      <div className="absolute top-2 right-4">
+        <StaticIcon icon={FaSearch} size={"small"} color={"text-gray-200"} />
+      </div>
     </div>
   );
 }

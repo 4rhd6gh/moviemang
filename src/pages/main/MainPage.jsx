2bc72381485ba20c66d14dbcbdab8d15e7eb45ff
Ji Hyuk Ma <t90laser93@gmail.com>
2022-04-21 16:47:32 +0900

    feat: appbar 컴포넌트 생성 및 페이지 레이아웃 수정

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -11,38 +11,35 @@ export default function MainPage() {
     // 전체 container
     <div className="container ml-auto mr-auto pt-28">
       {/* 상단 appbar */}
-      <header className="fixed top-0 w-full h-20 text-white bg-gray-600 appbar">
-        AppBar 영역
-      </header>
-      {/* <Appbar /> */}
+      <Appbar />
       {/* 인기태그 부분 */}
-      <div className="flex pl-6 text-2xl items-centerpt-4 w-[70%] mr-auto ml-auto pt-6">
+      <div className="flex pl-6 text-2xl items-center w-[1250px] mr-auto ml-auto pt-6">
         <h1 className="text-base">인기태그</h1>
-        <p className="ml-4 text-sm">#히어로</p>
-        <p className="ml-4 text-sm">#로맨스</p>
-        <p className="ml-4 text-sm">#스릴러</p>
+        <p className="ml-4 text-sm text-gray-400">#히어로</p>
+        <p className="ml-4 text-sm text-gray-400">#로맨스</p>
+        <p className="ml-4 text-sm text-gray-400">#스릴러</p>
       </div>
       {/* 영화목록 */}
-      <section className="h-[40vh] pt-4 bg-purple-300 w-[70%] mr-auto ml-auto">
+      <section className="h-[400px] pt-4 bg-purple-300 w-[1200px] mr-auto ml-auto">
         <h1 className="ml-6 text-2xl">좋아요 순</h1>
-        <div className="flex justify-between items-center mt-3 w-[90%] h-[30vh] bg-blue-300 ml-auto mr-auto pr-10 pl-10">
-          <div className="w-1/5 h-[27vh] border-white border-2"></div>
-          <div className="w-1/5 h-[27vh] border-white border-2"></div>
-          <div className="w-1/5 h-[27vh] border-white border-2"></div>
-          <div className="w-1/5 h-[27vh] border-white border-2"></div>
+        <div className="flex justify-between items-center mt-3 w-[90%] h-[300px] bg-blue-300 ml-auto mr-auto pr-10 pl-10">
+          <div className="w-1/5 h-[270px] border-white border-2"></div>
+          <div className="w-1/5 h-[270px] border-white border-2"></div>
+          <div className="w-1/5 h-[270px] border-white border-2"></div>
+          <div className="w-1/5 h-[270px] border-white border-2"></div>
         </div>
       </section>
-      <section className=" w-[70%] mr-auto ml-auto h-[40vh] pt-4 bg-purple-400">
+      <section className=" w-[1200px] mr-auto ml-auto h-[400px] pt-4 bg-purple-400">
         <h1 className="ml-6 text-2xl">태그기반 추천</h1>
-        <div className="flex justify-between items-center mt-3 w-[90%] h-[30vh] bg-blue-300 ml-auto mr-auto pr-10 pl-10">
-          <div className="w-1/5 h-[27vh] border-white border-2"></div>
-          <div className="w-1/5 h-[27vh] border-white border-2"></div>
-          <div className="w-1/5 h-[27vh] border-white border-2"></div>
-          <div className="w-1/5 h-[27vh] border-white border-2"></div>
+        <div className="flex justify-between items-center mt-3 w-[90%] h-[300px] bg-blue-300 ml-auto mr-auto pr-10 pl-10">
+          <div className="w-1/5 h-[270px] border-white border-2"></div>
+          <div className="w-1/5 h-[270px] border-white border-2"></div>
+          <div className="w-1/5 h-[270px] border-white border-2"></div>
+          <div className="w-1/5 h-[270px] border-white border-2"></div>
         </div>
       </section>
-      <aside className="w-full h-[15vh] flex items-center justify-center border-t-2 border-b-2">
-        <div className="text-2xl text-white">아이콘</div>
+      <aside className="w-full h-[150px] flex items-center justify-center border-t-2 border-b-2">
+        <div className="text-2xl text-blue-300">아이콘삽입</div>
         <p className="ml-10 text-2xl ">
           여름이 오면 가장 먼저 생각나는 오싹한 호러영화를 투표해주세요!
         </p>
@@ -50,13 +47,13 @@ export default function MainPage() {
           투표
         </button>
       </aside>
-      <section className="w-[70%] mr-auto ml-auto h-[40vh] pt-4 bg-purple-600">
+      <section className="w-[1200px] mr-auto ml-auto h-[400px] pt-4 bg-purple-300">
         <h1 className="ml-6 text-2xl">좋아요 순</h1>
-        <div className="flex justify-between items-center mt-3 w-[90%] h-[30vh] bg-blue-300 ml-auto mr-auto pr-10 pl-10">
-          <div className="w-1/5 h-[27vh] border-white border-2"></div>
-          <div className="w-1/5 h-[27vh] border-white border-2"></div>
-          <div className="w-1/5 h-[27vh] border-white border-2"></div>
-          <div className="w-1/5 h-[27vh] border-white border-2"></div>
+        <div className="flex justify-between items-center mt-3 w-[90%] h-[300px] bg-blue-300 ml-auto mr-auto pr-10 pl-10">
+          <div className="w-1/5 h-[270px] border-white border-2"></div>
+          <div className="w-1/5 h-[270px] border-white border-2"></div>
+          <div className="w-1/5 h-[270px] border-white border-2"></div>
+          <div className="w-1/5 h-[270px] border-white border-2"></div>
         </div>
       </section>
       <div className="w-full h-20 text-white bg-gray-400">분리영역</div>

bd5712c3138baa03e3efed1eeb8d470c70c70ac5
Ji Hyuk Ma <t90laser93@gmail.com>
2022-04-23 08:16:07 +0900

    style: 예시 디자인 작성
    이런 느낌으로 만들어볼까하고 대략적으로 구성해보았습니다. 백엔드 분들도 보시고 어느정도 이런느낌이구나 하실 수있게 만들어보고자 하였습니다.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,28 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        headerbg: "#417D7A",
+        textMainColor: "white",
+        textSideColor: "#a5a5a5",
+        baseColor: "#1b1b1b;",
+        CardBgColor: "#DDDDDD",
+        CardBgColor2: "#FFEEEE",
+        CardBgColor3: "#EEF2FF",
+        CardBgColor4: "#3A3845",
+        CardBgColor5: "#B7CADB",
+        CardBgColor6: "#F7F7F7",
+        CardBgColor7: "#789395",
+        CardBgColor8: "#2C3333",
+        CardBgColor9: "#FFAFAF",
+      },
+    },
     screens: {
       mobile: { max: "640px" },
       // => @media (max-width: 640px) { ... }
 
-      tablet: { max: "1024px" },
+      tablet: { max: "1200px" },
       // => @media (max-width: 1024px) { ... }
     },
   },

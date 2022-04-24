8d2d55d8d24a41499b40a5120df0cb4431842045
ssun-aa <ssk6200@daum.net>
2022-04-24 18:46:41 +0900

    feat: toggleSwitch 컴포넌트 및 스토리북 생성

---
+++ src/pages/common/toggleSwitch/index.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+export default function ToggleSwitch(props) {
+  const [toggle, setToggle] = useState(true);
+
+  return (
+    <div
+      className={
+        "md:w-14 md:h-7 w-12 h-6 flex items-center rounded-full p-1 cursor-pointer " +
+        (toggle ? "bg-gray-300/50" : "bg-blue-300/50 ")
+      }
+      onClick={() => {
+        setToggle(!toggle);
+      }}
+    >
+      <div
+        className={
+          "bg-white md:w-6 md:h-6 h-5 w-5 rounded-full shadow-md " +
+          (toggle ? "bg-white" : "bg-blue-400 transform translate-x-5")
+        }
+      ></div>
+    </div>
+  );
+}
+
+ToggleSwitch.propTypes = {};

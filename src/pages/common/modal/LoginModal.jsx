e3b6a5c4434f277b3f3d5537b9426b3c9bf1fbd1
bgradecoding <iinee12@kakao.com>
2022-05-01 10:44:45 +0900

    feat(loginmodal.jsx): modal 외부 영역을 클릭했을때 창 닫히도록 기능 추가

--- src/pages/common/modal/LoginModal.jsx
+++ src/pages/common/modal/LoginModal.jsx
@@ -14,18 +14,30 @@ export default function LoginModal(props) {
     password: Yup.string().required("패스워드를 입력하지 않았습니다."),
   });
 
+  const onCloseModal = (e) => {
+    console.log("e.target: ", e.target.type);
+    console.log("e.tarcurrentTargetget: ", e.currentTarget.type);
+    console.log(e.target === e.currentTarget && e.target.type !== "submit");
+    if (e.target === e.currentTarget) {
+      onClose(false);
+    }
+  };
+
   return (
     <>
       {open ? (
         <>
-          <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
+          <div
+            className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none"
+            onClick={onCloseModal}
+          >
             <div className="relative w-auto max-w-3xl mx-auto my-6">
               <div className="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none">
                 <div className="flex items-start justify-between p-5 border-b border-solid rounded-t border-slate-200">
                   <h3 className="text-3xl font-semibold">Login</h3>
                   <button
                     className="float-right p-1 ml-auto text-3xl font-semibold leading-none text-black bg-transparent border-0 outline-none opacity-5 focus:outline-none"
-                    onClick={() => onClose(false)}
+                    onClick={onCloseModal}
                   >
                     <span className="block w-6 h-6 text-2xl text-black bg-transparent outline-none opacity-5 focus:outline-none">
                       ×
@@ -40,7 +52,6 @@ export default function LoginModal(props) {
                   validationSchema={LoginSchema}
                   onSubmit={(values) => {
                     // same shape as initial values
-                    onClose(false);
                     console.log(values);
                   }}
                 >

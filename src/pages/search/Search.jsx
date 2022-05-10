f62fb767621ccb0cbb3f2b932880a1f575e9f25e
ssun-aa <ssk6200@daum.net>
2022-05-11 00:11:22 +0900

    feat: spinner 컴포넌트 생성

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Appbar from "@page/common/appbar/Appbar";
+import Spinner from "@page/common/spinner";
 import * as MovieService from "@api/tmMovie/movie";
 import MovieSearchCard from "./components/movieSearchCard";
 import * as Constants from "@constant";
@@ -7,9 +8,11 @@ import { useLocation } from "react-router-dom";
 
 export default function MovieList() {
   const [showModal, setShowModal] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [movieList, setMovieList] = useState([]);
 
   const location = useLocation();
+  const value = location.state === null ? "" : location.state.value;
 
   async function getSearchMovieList() {
     const response = await MovieService.getSearchMovieList(
@@ -17,15 +20,17 @@ export default function MovieList() {
       "search/movie",
       {},
       1,
-      location.state.value
+      value
     );
     const movies = response.results;
     setMovieList(movies);
+    setLoading(false);
   }
 
   useEffect(() => {
+    setLoading(true);
     getSearchMovieList();
-  }, [location.state.value]);
+  }, [value]);
 
   return (
     <div className="container ml-auto mr-auto pt-28 ">
@@ -39,23 +44,27 @@ export default function MovieList() {
           </div>
         </div>
         <div className="flex justify-center w-full">
-          <div className=" w-[70%]">
-            <div className="flex flex-wrap flex-col justify-between ml-16 mr-16">
-              {movieList.map((movie) => (
-                <div className="py-5 w-full">
-                  <MovieSearchCard
-                    key={movie.title}
-                    poster_path={
-                      Constants.TM_MOVIE_IMAGE_URL + movie.poster_path
-                    }
-                    title={movie.title}
-                    release_date={movie.release_date}
-                    overview={movie.overview}
-                  />
-                </div>
-              ))}
+          {loading ? (
+            <Spinner />
+          ) : (
+            <div className=" w-[70%]">
+              <div className="flex flex-wrap flex-col justify-between ml-16 mr-16">
+                {movieList.map((movie) => (
+                  <div className="py-5 w-full">
+                    <MovieSearchCard
+                      key={movie.title}
+                      poster_path={
+                        Constants.TM_MOVIE_IMAGE_URL + movie.poster_path
+                      }
+                      title={movie.title}
+                      release_date={movie.release_date}
+                      overview={movie.overview}
+                    />
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </div>

21f2dd425a53987342d50908ef54609a219abd59
ssun-aa <ssk6200@daum.net>
2022-07-05 16:23:33 +0900

    feat: redux-persist

--- src/data/rootReducer.js
+++ src/data/rootReducer.js
@@ -1,15 +1,22 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import user from "./user/reducers";
 import movie from "./movie/reducers";
 import search from "./search/reducers";
 import common from "./common/reducers";
 
-const createRootReducer = () =>
-  combineReducers({
-    user,
-    movie,
-    search,
-    common,
-  });
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["user"],
+};
 
-export { createRootReducer };
+export const rootReducer = combineReducers({
+  user,
+  movie,
+  search,
+  common,
+});
+export default persistReducer(persistConfig, rootReducer);

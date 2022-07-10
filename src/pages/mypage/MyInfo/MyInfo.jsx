c9c8b67cc181e3508e425c516151168338ae1d10
Jihyuk Ma <t90laser93@gmail.com>
2022-07-10 13:07:36 +0900

    refactor(닉네임 수정): 닉네임 수정 관련 함수 이름 수정 및 계정 표시 UI 텍스트 줄바꿈 안되도록 수정

--- src/pages/mypage/MyInfo/MyInfo.jsx
+++ src/pages/mypage/MyInfo/MyInfo.jsx
@@ -11,7 +11,6 @@ import Chart from "./chart";
 import PlayListCard from "@page/common/playListCard";
 import * as Mock from "@data/mock";
 import { BsPersonFill as PersonIcon } from "react-icons/bs";
-import * as apis from "@service/apis/movieMang";
 
 export default function MyPage() {
   const userData = useSelector(selector.user.getUserData);
@@ -19,34 +18,40 @@ export default function MyPage() {
 
   const dispatch = useDispatch();
 
+  const currentUserNickName = localStorage.getItem("nickname");
+
+  console.log(currentUserNickName);
+
   function loginType() {
     let sns = userData.loginType;
+    console.log(sns);
     if (sns === "kakao") {
       return (
-        <div className="w-[94px] py-2 px-3 mt-2 rounded bg-[#FEE500]">
+        <div className="w-[94px] py-2 px-3 mt-2 rounded bg-[#FEE500] whitespace-pre">
           Kakao 계정
         </div>
       );
     } else if (sns === "naver") {
       return (
-        <div className="w-[91px] py-2 px-3 mt-2 rounded text-white bg-[#18CE5F]">
+        <div className="w-[91px] py-2 px-3 mt-2 rounded text-white bg-[#18CE5F] whitespace-pre">
           Naver 계정
         </div>
       );
     } else {
       return (
-        <div className="w-[98px] py-2 px-3 mt-2 rounded text-white bg-[#4285F4]">
+        <div className="w-[98px] py-2 px-3 mt-2 rounded text-white bg-[#4285F4] whitespace-pre">
           Google 계정
         </div>
       );
     }
   }
 
-  function handleChange(e) {
+  function handleNickNameInputChange(e) {
     setNickName(e.currentTarget.value);
   }
 
-  const handleButtonClick = async () => {
+  const handleNickNameChangeRequest = async (e) => {
+    e.preventDefault();
     await dispatch(
       action.user.editNickName({
         nickname,
@@ -59,12 +64,12 @@ export default function MyPage() {
       <div className="w-[20%]">
         <SNB />
       </div>
-      <div className=" p-10 max-w-2xl w-full">
+      <div className="w-full max-w-2xl p-10 ">
         <div className="flex">
-          <div className="w-52 h-52 bg-gray-400 rounded">
+          <div className="bg-gray-400 rounded w-52 h-52">
             <PersonIcon className="w-52 h-52" fill="#d1d5db" />
           </div>
-          <div className="mx-5 my-2">
+          <form className="mx-5 my-2" onSubmit={handleNickNameChangeRequest}>
             <Input
               type="text"
               value={nickname}
@@ -76,32 +81,31 @@ export default function MyPage() {
               border="border-solid border border-slate-400"
               textColor="text-textMainColor"
               backgroundColor="bg-searchBarBackgroundColor"
-              onChange={handleChange}
+              onChange={handleNickNameInputChange}
             />
             <Button
               text="수정"
-              type="button"
+              type="submit"
               color="text-white"
               width="w-[80px]"
               height="h-[38px]"
               backgroundColor="bg-themePink"
               borderRadius="rounded"
-              onClick={handleButtonClick}
-            ></Button>
+            />
             {loginType()}
-          </div>
+          </form>
         </div>
         <div className="my-10">
           <Chart />
         </div>
         <div>
-          <h1 className="text-xl font-bold  text-textMainColor">
+          <h1 className="text-xl font-bold text-textMainColor">
             나의 플레이리스트
           </h1>
           <div className="float-right mb-4 text-xs text-textMainColor">
             <NavLink to={"playlist"}>전체보기</NavLink>
           </div>
-          <div className="flex w-full justify-between items-center">
+          <div className="flex items-center justify-between w-full">
             {Mock.playList.playList.slice(0, 2).map((movie, index) => {
               return (
                 <div key={index}>

9ef9ed29157e5971bf3c422d11474aac01b932ef
Jihyuk Ma <t90laser93@gmail.com>
2022-07-11 15:11:19 +0900

    style(appbar 길이 조정): appBar 길이 조정 및 관련 style 수정
    AppBar 길이가 짧아보이던 문제 수정하였습니다. 그리고 AppBar 및 SearchBar 부분 css 조금씩 수정하였습니다.

--- src/pages/common/appbar/SearchBar/index.jsx
+++ src/pages/common/appbar/SearchBar/index.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
-import StaticIcon from "@component/Icons/StaticIcon";
+import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+
+import StaticIcon from "@component/Icons/StaticIcon";
 import Input from "@component/Input";
 import Button from "@component/Button";
 
@@ -15,6 +16,7 @@ export default function SearchBar() {
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
+
     if (value === "") return;
 
     navigate("/search/" + value);
@@ -22,39 +24,40 @@ export default function SearchBar() {
   };
 
   return (
-    <div className="relative mt-[10px] ml-10 w-[350px]  tablet:w-[220px] tablet:ml-6 md:hidden  mobile:hidden">
-      <form id="searchForm" onSubmit={handleFormSubmit}>
-        <Input
-          type="search"
-          value={value}
-          placeholder="보고 싶은 영화를 찾아보세요"
-          width="w-full"
-          padding="px-5 py-2 pr-10"
-          margin="mr-3"
-          borderRadius="rounded-full"
-          border="border-solid border border-slate-400"
-          textColor="text-textMainColor"
-          backgroundColor="bg-searchBarBackgroundColor"
-          onChange={handleChangeSearch}
+    <form
+      id="searchForm"
+      onSubmit={handleFormSubmit}
+      className="relative mt-[10px] ml-20 w-[350px] tablet:w-[270px] tablet:ml-10 md:hidden mobile:hidden"
+    >
+      <Input
+        type="search"
+        value={value}
+        placeholder="보고 싶은 영화를 찾아보세요"
+        width="w-full"
+        padding="px-5 py-2 pr-10"
+        margin="mr-3"
+        borderRadius="rounded-full"
+        border="border-solid border border-slate-400"
+        textColor="text-textMainColor"
+        backgroundColor="bg-searchBarBackgroundColor"
+        onChange={handleChangeSearch}
+      />
+      <div className="absolute top-2 right-4 ">
+        <Button
+          type="submit"
+          form="searchForm"
+          width="w-6"
+          height="h-6"
+          margin="m-0"
+          text={
+            <StaticIcon
+              icon={FaSearch}
+              size={"small"}
+              color={"text-gray-200"}
+            />
+          }
         />
-
-        <div className="absolute top-2 right-4 ">
-          <Button
-            type="submit"
-            form="searchForm"
-            width="w-6"
-            height="h-6"
-            margin="m-0"
-            text={
-              <StaticIcon
-                icon={FaSearch}
-                size={"small"}
-                color={"text-gray-200"}
-              />
-            }
-          ></Button>
-        </div>
-      </form>
-    </div>
+      </div>
+    </form>
   );
 }

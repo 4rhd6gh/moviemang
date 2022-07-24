064b185a004714bf2bc5069e26cfd17819ef36e2
ssun-aa <ssk6200@daum.net>
2022-07-24 23:21:57 +0900

    refactor: remove not used constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -12,13 +12,11 @@ export const TM_API_KEY = "4d48c467d91cd60bbd7ef0bffa7b1a73";
 // 카카오 로그인 구현 시 필요한 키값
 const KAKAO_CLIENT_ID = "7b3b8e4bac82885a106b3d0eac76cd86";
 const KAKAO_REDIRECT_URI = "http://localhost:3000/oauth/kakao";
-const KAKAO_CLIENT_SECRET = "6LX0v5unJyJuRvSwlnQejgoqE3FgzBFX";
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
 
 // 네이버 로그인 구현 시 필요한 키값
 const NAVER_CLIENT_ID = "P9wXmSD_A0d2dUt21vm0";
-const NAVER_CLIENT_SECRET = "YIYyWZR319";
 const NAVER_REDIRECT_URI = "http://localhost:3000/oauth/naver";
 
 const STATE_STRING = encodeURI("accc", "UTF-8");

2e50760914335a66ff1d8fbed66e180bb8596ee4
ssun-aa <ssk6200@daum.net>
2022-08-09 18:07:32 +0900

    feat: mypage tag chart

--- src/pages/mypage/MyInfo/chart/index.jsx
+++ src/pages/mypage/MyInfo/chart/index.jsx
@@ -1,41 +1,84 @@
-import React from "react";
-import { data } from "../../data";
+import React, { useEffect } from "react";
+import * as apis from "@service/apis/movieMang";
+import Highcharts from "highcharts";
 
 function Chart() {
-  const playList = data.data.playlist;
-  const tagList = playList.reduce(function (accumulator, currentValue) {
-    return [...accumulator, ...currentValue.tags];
-  }, []);
-  const tags = [];
-  tagList.forEach((tag) => {
-    let id = tags.findIndex((obj) => obj.name === tag.name);
-    if (id === -1) {
-      tags.push({ name: tag.name, count: 1, percent: 0 });
-    } else {
-      tags[id].count++;
+  async function getChartData() {
+    let response;
+    let totalCNT = 0;
+    try {
+      response = await apis.requestAxios("get", "/user/playlistTags");
+      totalCNT = response.data.tags.reduce((p, c) => p + c.CNT, 0);
+      const tags = response.data.tags.map((tag, i) => {
+        return { name: tag.tagName, y: (tag.CNT / totalCNT) * 100 };
+      });
+      drawChart(tags);
+    } catch (err) {
+      console.log(err);
     }
-  });
-  tags.map((tag, i) => {
-    tags[i].percent = (tags[i].count / tagList.length) * 100;
-    return tag;
-  });
+  }
+
+  useEffect(() => {
+    getChartData();
+  }, []);
+
+  function drawChart(tags) {
+    Highcharts.chart("container", {
+      chart: {
+        type: "column",
+        backgroundColor: "transparent",
+      },
+      title: {
+        align: "left",
+        text: null,
+      },
+      accessibility: {
+        announceNewData: {
+          enabled: false,
+        },
+      },
+      xAxis: {
+        type: "category",
+      },
+      yAxis: {
+        title: null,
+      },
+      legend: {
+        enabled: false,
+      },
+      plotOptions: {
+        series: {
+          borderWidth: 0,
+          dataLabels: {
+            enabled: false,
+          },
+        },
+      },
+
+      tooltip: {
+        headerFormat: null,
+        pointFormat:
+          '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.1f}%</b>',
+      },
+
+      series: [
+        {
+          name: "Tags",
+          colorByPoint: true,
+          data: tags,
+        },
+      ],
+    });
+  }
 
   return (
     <div className="w-full mx-auto">
       <h1 className="text-xl mb-2 font-bold text-l text-textMainColor">
         나의 플레이리스트 태그 통계
       </h1>
-      <div className="flex flex-col">
-        {tags.map((tag, i) => (
-          <div key={i} className="flex mb-1">
-            <div className="text-white w-20">#{tag.name}</div>
-            <div className="my-auto bg-gray-600 w-full">
-              <div className={`h-1 bg-pink-400 w-[${tag.percent}%]`} />
-            </div>
-            <div className="text-white pl-3">{tag.percent}%</div>
-          </div>
-        ))}
-      </div>
+      <figure className="highcharts-figure">
+        <div id="container"></div>
+      </figure>
     </div>
   );
 }

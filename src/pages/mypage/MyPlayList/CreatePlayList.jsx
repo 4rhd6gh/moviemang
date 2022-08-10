6ec9025f76c1291762f1abca2b324ba247b61eb9
Jihyuk Ma <t90laser93@gmail.com>
2022-08-10 17:03:49 +0900

    feat: input/textarea에 label 및 스타일링 추가

--- src/pages/mypage/MyPlayList/CreatePlayList.jsx
+++ src/pages/mypage/MyPlayList/CreatePlayList.jsx
@@ -94,28 +94,47 @@ export default function CreatePlayList() {
   return (
     <main className="flex justify-center w-full">
       <div className="flex flex-col justify-center w-[60%] p-10 border-4 border-solid rounded-b border-[#0E2133] bg-[#0C1A2A] ">
-        <h2 className="mb-5 text-textMainColor">플레이리스트 생성하기</h2>
-        <div className="flex flex-col justify-center ">
+        <h2 className="mb-5 text-xl text-textMainColor">
+          플레이리스트 생성하기
+        </h2>
+        <div className="flex flex-col justify-center mt-5">
           <form id="playlistForm" onSubmit={handleSubmit}>
-            <Input
-              type="text"
-              placeholder="제목"
-              width="w-full"
-              padding="p-3"
-              margin="mb-3"
-              textColor="text-textMainColor"
-              backgroundColor="bg-[#233A50]"
-              borderRadius="rounded-lg"
-              onChange={handleTitleChange}
-              required={true}
-            />
-            <textarea
-              row={10}
-              placeholder="상세 설명"
-              onChange={handleContentsChange}
-              required
-              className="w-full p-3 mb-3 bg-[#233A50] text-textMainColor rounded-lg h-40 resize-none"
-            />
+            <div className="relative">
+              <Input
+                inputName="titleInput"
+                type="text"
+                width="w-full"
+                padding="p-3"
+                margin="mb-3"
+                textColor="text-textMainColor"
+                backgroundColor="bg-[#233A50]"
+                borderRadius="rounded-lg"
+                onChange={handleTitleChange}
+                required={true}
+              />
+              <label
+                htmlFor="titleInput"
+                className="absolute text-white transition-all left-3 top-3 peer-focus:-top-6 peer-focus:left-0 peer-focus:text-sm peer-valid:-top-6 peer-valid:left-0 peer-valid:text-sm"
+              >
+                제목
+              </label>
+            </div>
+            <div className="relative">
+              <textarea
+                name="descriptionTextArea"
+                row={10}
+                onChange={handleContentsChange}
+                required
+                className="mt-6 w-full p-3 mb-3 bg-[#233A50] text-textMainColor rounded-lg h-40 resize-none focus:outline-none peer"
+              />
+              <label
+                htmlFor="descriptionTextArea"
+                className="absolute text-white transition-all left-3 top-8 peer-focus:top-0 peer-focus:left-0 peer-focus:text-sm peer-valid:top-0 peer-valid:left-0 peer-valid:text-sm"
+              >
+                상세 설명
+              </label>
+            </div>
+
             <div className="flex flex-row flex-wrap justify-center gap-2 p-3">
               {Tags()}
             </div>
